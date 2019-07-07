81610eec779e99dc513ae8335629e8df11d16c32
gcmatheusj <goca1377@gmail.com>
2019-07-07 19:54:53 -0300

    create user and session

---
+++ NodeJS/GoBarber/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '7031e35cd7ecb9d85455e8bdae4b349c',
+  expiresIn: '7d',
+};
--- NodeJS/GoBarber/src/routes.js
+++ NodeJS/GoBarber/src/routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (re, res) =>
-  res.json({
-    message: 'hello teste',
-  })
-);
+routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
 
 export default routes;
