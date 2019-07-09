2d9365ee2f100240448339dd59399460159d62d3
gcmatheusj <goca1377@gmail.com>
2019-07-08 22:01:11 -0300

    auth middleware and update user

--- NodeJS/GoBarber/src/app/controllers/UserController.js
+++ NodeJS/GoBarber/src/app/controllers/UserController.js
@@ -5,10 +5,29 @@ class UserController {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
-      return res.json({ error: 'User already exists.' });
+      return res.status(400).json({ error: 'User already exists.' });
     }
 
     const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Invalid password.' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
 
     return res.json({
       id,
