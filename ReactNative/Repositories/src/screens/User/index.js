660eaf191704a1717aec79b1bedf5d3fdf54444f
gcmatheusj <goca1377@gmail.com>
2019-07-14 11:54:37 -0300

    add pull to refresh and infinity scroll

--- ReactNative/Repositories/src/screens/User/index.js
+++ ReactNative/Repositories/src/screens/User/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 
 import api from '../../services/api';
@@ -9,6 +10,7 @@ import {
   Avatar,
   Name,
   Bio,
+  Loading,
   Stars,
   Starred,
   OwnerAvatar,
@@ -19,18 +21,48 @@ import {
 
 export default function User({ navigation }) {
   const [stars, setStars] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [page, setPage] = useState(2);
 
   async function getStarredRepositories() {
+    setLoading(true);
+
     const user = navigation.getParam('user');
 
     const response = await api.get(`/users/${user.login}/starred`);
 
     setStars(response.data);
+    setLoading(false);
   }
 
   useEffect(() => {
     getStarredRepositories();
   }, []);
+
+  async function loadMoreStarredRepositories() {
+    setLoadingMore(true);
+
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred?page=${page}`);
+
+    setStars([...stars, ...response.data]);
+    setPage(page + 1);
+    setLoadingMore(false);
+  }
+
+  function renderFooter() {
+    if (!loadingMore) {
+      return null;
+    }
+
+    return (
+      <Loading>
+        <ActivityIndicator color="#039be5" size="small" />
+      </Loading>
+    );
+  }
 
   const user = navigation.getParam('user');
 
@@ -42,19 +74,30 @@ export default function User({ navigation }) {
         <Bio>{user.bio}</Bio>
       </Header>
 
-      <Stars
-        data={stars}
-        keyExtractor={star => String(star.id)}
-        renderItem={({ item }) => (
-          <Starred>
-            <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-            <Info>
-              <Title>{item.name}</Title>
-              <Author>{item.owner.login}</Author>
-            </Info>
-          </Starred>
-        )}
-      />
+      {loading ? (
+        <Loading>
+          <ActivityIndicator color="#039be5" size="large" />
+        </Loading>
+      ) : (
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          onRefresh={getStarredRepositories}
+          refreshing={loading}
+          onEndReachedThreshold={0.2}
+          onEndReached={loadMoreStarredRepositories}
+          ListFooterComponent={renderFooter}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      )}
     </Container>
   );
 }
