af83875a3c73a30d582ac72d51bd4ca71a1557a4
gcmatheusj <goca1377@gmail.com>
2019-07-14 19:21:31 -0300

    add schedule and appointment controller

--- NodeJS/GoBarber/src/routes.js
+++ NodeJS/GoBarber/src/routes.js
@@ -6,6 +6,8 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import AuthMiddleware from './app/middlewares/Auth';
 
@@ -18,6 +20,11 @@ routes.post('/session', SessionController.store);
 routes.use(AuthMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.get('/providers', ProviderController.index);
 
