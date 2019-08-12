9bc1e460afcef6db3deee8d16abb2dcd1f93ae47
gcmatheusj <goca1377@gmail.com>
2019-08-11 23:38:54 -0300

    repo(gobarber): send email

---
+++ NodeJS/GoBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'e45bc0ebbd15c6',
+    pass: '440435b58e57ca',
+  },
+  default: {
+    from: 'Equipe Consulte Aqui <noreply@consulteaqui.com',
+  },
+};
